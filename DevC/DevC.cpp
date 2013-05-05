8f3a23d66fe0a5016bbf04c7708ab14af144cf82
nawal addou <nawal.addou@gmail.com>
2013-05-05 04:34:45 +0100

    projet1

---
+++ DevC/DevC.cpp
@@ -0,0 +1,19 @@
+// DevC.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <stdio.h>  /* Autorise l'emploi de printf et de scanf. */
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+long double x, y;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello world!" << std::endl;
+
+    int age;
+    cin >> age;
+}
+
