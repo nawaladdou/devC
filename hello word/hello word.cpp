f979a9703a14090c2899d55df10d0200d3d10f35
nawal addou <nawal.addou@gmail.com>
2013-05-07 22:48:21 +0100

    Hello word

---
+++ hello word/hello word.cpp
@@ -0,0 +1,14 @@
+// hello word.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
+
